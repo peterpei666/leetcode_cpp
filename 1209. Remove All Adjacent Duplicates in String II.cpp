c248baaa9859c979711500f1046aba8a2eaa3f09
Peter_Pei <mmiaomiao140@gmail.com>
2025-06-07 23:34:25 +0800

    Add files via upload

---
+++ 1209. Remove All Adjacent Duplicates in String II.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeDuplicates(const string& s, int k)
+    {
+        stack<pair<char,int>> list;
+        for(char c:s)
+        {
+            if(list.empty()||list.top().first!=c)
+            {
+                list.push({c,1});
+            }
+            else
+            {
+                auto& [_,n]=list.top();
+                n++;
+                if(n==k)
+                {
+                    list.pop();
+                }
+            }
+        }
+        string ret="";
+        while(!list.empty())
+        {
+            auto [c,n]=list.top();
+            ret+=string(n,c);
+            list.pop();
+        }
+        reverse(ret.begin(),ret.end());
+        return ret;
+    }
+};
