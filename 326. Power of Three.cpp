b9ef15c25fc6e564dec1accb44b7d5a7b1575d31
Peter_Pei <mmiaomiao140@gmail.com>
2025-08-13 23:33:40 +0800

    Add files via upload

---
+++ 326. Power of Three.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        while(n%3==0)
+        {
+            n/=3;
+        }
+        return n==1;
+    }
+};
