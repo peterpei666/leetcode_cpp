51a8b7e5f2f4c055144fd8519c2e0234baf92316
Peter_Pei <mmiaomiao140@gmail.com>
2025-09-08 23:14:27 +0800

    Add files via upload

---
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+private:
+    static inline bool check(int x)
+    {
+        while (x)
+        {
+            if (x % 10 == 0)
+            {
+                return false;
+            }
+            x /= 10;
+        }
+        return true;
+    }
+    
+public:
+    vector<int> getNoZeroIntegers(int n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if (check(i) && check(n - i))
+            {
+                return {i, n - i};
+            }
+        }
+        return {};
+    }
+};
