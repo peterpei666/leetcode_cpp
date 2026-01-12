604223fb6d1ee3b948608870c77ef98b2d18c10b
Peter_Pei <mmiaomiao140@gmail.com>
2026-01-12 22:43:24 +0800

    Add files via upload

--- 1266. Minimum Time Visiting All Points.cpp
+++ 1266. Minimum Time Visiting All Points.cpp
@@ -7,11 +7,11 @@ class Solution
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points)
     {
-        int ans=0;
-        int n=(int)points.size();
-        for(int i=1;i<n;i++)
+        int n = (int)points.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++)
         {
-            ans+=max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]));
+            ans += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
         }
         return ans;
     }
