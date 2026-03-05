cd436961b5d062edb093b373d069b1dad9a5adea
Peter_Pei <mmiaomiao140@gmail.com>
2026-03-05 23:24:35 +0800

    Add files via upload

---
+++ 1032. Stream of Characters.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class StreamChecker
+{
+    
+private:
+    struct node
+    {
+        node* child[26];
+        bool tag;
+        
+        node() { memset(child, 0, sizeof(child)); tag = false; }
+    };
+    
+    int n;
+    node* root;
+    string s;
+    
+    void insert(const string& s)
+    {
+        node* temp = root;
+        for (const char& c : s)
+        {
+            if (temp->child[c - 'a'] == nullptr)
+            {
+                temp->child[c - 'a'] = new node();
+            }
+            temp = temp->child[c - 'a'];
+        }
+        temp->tag = true;
+    }
+    
+    void clear(node* temp)
+    {
+        for (int i = 0; i < 52; i++)
+        {
+            if (temp->child[i])
+            {
+                clear(temp->child[i]);
+            }
+        }
+        delete temp;
+    }
+    
+public:
+    StreamChecker(vector<string>& words)
+    {
+        n = 0;
+        root = new node();
+        for (auto& s : words)
+        {
+            reverse(s.begin(), s.end());
+            insert(s);
+        }
+    }
+    
+    bool query(char letter)
+    {
+        s.push_back(letter);
+        n++;
+        node* temp = root;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            char c = s[i];
+            if (temp->child[c - 'a'] == nullptr)
+            {
+                return false;
+            }
+            temp = temp->child[c - 'a'];
+            if (temp->tag)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    ~StreamChecker()
+    {
+        clear(root);
+    }
+};
