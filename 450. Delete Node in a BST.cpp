6bc858b5b4e810af9c946009b8bc226f613a339a
Peter_Pei <mmiaomiao140@gmail.com>
2026-03-10 22:59:10 +0800

    Add files via upload

---
+++ 450. Delete Node in a BST.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode* deleteNode(TreeNode* root, int key)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+        if (key < root->val)
+        {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->val)
+        {
+            root->right = deleteNode(root->right, key);
+        }
+        else
+        {
+            if (root->left == nullptr && root->right == nullptr)
+            {
+                return nullptr;
+            }
+            if (root->left == nullptr || root->right == nullptr)
+            {
+                return root->left == nullptr ? root->right : root->left;
+            }
+            TreeNode* temp = root->left;
+            while (temp->right)
+            {
+                temp = temp->right;
+            }
+            root->val = temp->val;
+            root->left = deleteNode(root->left, temp->val);
+        }
+        return root;
+    }
+};
