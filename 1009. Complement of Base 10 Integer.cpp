a1bbb3622c23e2a9878e9516a9ff6acd073e329c
Peter_Pei <mmiaomiao140@gmail.com>
2026-03-11 23:37:09 +0800

    Add files via upload

---
+++ 1009. Complement of Base 10 Integer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int bitwiseComplement(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        int ans = 0;
+        for (int i = 0; (1 << i) <= n; i++)
+        {
+            if ((n & (1 << i)) == 0)
+            {
+                ans |= 1 << i;
+            }
+        }
+        return ans;
+    }
+};
---
+++ 476. Number Complement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int findComplement(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        int ans = 0;
+        for (int i = 0; i < 31 && (1 << i) <= n; i++)
+        {
+            if ((n & (1 << i)) == 0)
+            {
+                ans |= 1 << i;
+            }
+        }
+        return ans;
+    }
+};
